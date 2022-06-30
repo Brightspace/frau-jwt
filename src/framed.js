62fd93a6598c4203961a9edb926b38d2170ee44f
Dave Lockhart <dlockhart@users.noreply.github.com>
2022-06-30 08:00:00 -0400

    remove space
    Co-authored-by: Stacey Van Herk <Stacey.VanHerk@d2l.com>

--- src/framed.js
+++ src/framed.js
@@ -3,7 +3,7 @@ import REQUEST_KEY from './request-key.js';
 
 export default async function getFramedJwt(scope, opts) {
 	const Client = await clientPromise;
-	const client = await  new Client().connect();
+	const client = await new Client().connect();
 	const token = await client.request(REQUEST_KEY, scope, opts);
 	return token;
 }
